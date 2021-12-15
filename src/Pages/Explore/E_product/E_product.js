1758d3418c6b6e6f8f14f069b8bf14a74e9f4cdf
mizan <dewan.mizanur911@gmail.com>
2021-12-15 16:22:03 +0600

    e_product page updated with react rating and other informations

--- src/Pages/Explore/E_product/E_product.js
+++ src/Pages/Explore/E_product/E_product.js
@@ -2,37 +2,46 @@ import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import './E_product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import Rating from 'react-rating';
 
-const E_product = () => {
-    const element = <FontAwesomeIcon icon={faCoffee} />
+const E_product = ({product}) => {
+    const heart = <FontAwesomeIcon icon={faHeart} />
+    const cart = <FontAwesomeIcon icon={faCartPlus} />
+    const {img, name, price, stock, star} = product;
     return (
         <Col>
         <Card className="p-3">
             <Card.Text>
                 <div className="d-flex justify-content-between">
-                    <div className="bg-dark text-white px-2">
+                    <div className="bg-danger text-white px-2">
                         <span>5% off</span>
                     </div>
-                    <span className="px-2">{element}</span>
+                    <span className="far fa-heart favorite"></span>
                 </div>
             </Card.Text>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={img} height="140" />
             <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>{name.slice(0,24)}</Card.Title>
             <Card.Text>
                 <div className="d-flex justify-content-between">
                     <div className="text-start">
-                        <p><small>Price: $24</small></p>
-                        <p><small>Stock : 35 items</small></p>
+                        <p><small>Price: ${price}</small></p>
+                        <p><small>Stock : {stock} items</small></p>
                     </div>
                     <div>
-                        <p><small>{element}</small></p>
-                        <p><small>{element}</small></p>
+                        <p><small><Rating
+                                initialRating={star}
+                                readonly
+                                emptySymbol="far fa-star icon-color"
+                                fullSymbol="fas fa-star icon-color"
+                                />
+                            </small>
+                        </p>
                     </div>
                 </div>
             </Card.Text>
-            <button className="btn-products">Order Now</button>
+            <button className="btn-products"><span className="me-2">{cart}</span>Order Now</button>
             </Card.Body>
         </Card>
         </Col>
