a73a5e53b743f32c7ab98f764507055ba8967c17
mizan <dewan.mizanur911@gmail.com>
2021-12-25 01:03:47 +0600

    My orders page completed

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Spinner, Table } from 'react-bootstrap';
+import useAuth from '../../../hooks/useAuth';
 import './MyOrders.css';
 
 const MyOrders = () => {
+    const { user } = useAuth();
+    const [myOrders, setMyOrders] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyOrders(data))
+    }, [user.email]);
+
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Confirm delete your order?')
+        if (proceed) {
+            const uri = `http://localhost:5000/myOrders/${id}`;
+            fetch(uri, {
+                method: "DELETE",
+            })
+                .then((res) => res.json)
+                .then((data) => {
+                    const restOrders = myOrders.filter(order => order._id !== id)
+                    setMyOrders(restOrders);
+                });
+        }
+
+    };
+
     return (
         <div>
-            <h2>My Orders page</h2>
+            <h5>My Orders</h5>
+            <Table responsive striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Ordered Time</th>
+                        <th>Payment</th>
+                        <th>Status</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        myOrders.length ? myOrders.map(order => <tr key={order._id}>
+                            <td>{order.buyerName}</td>
+                            <td>{order.email}</td>
+                            <td>{order.product}</td>
+                            <td>{order.price}</td>
+                            <td>{order.date}</td>
+                            <td>{order.payment?.created ? 'Paid' : 'Not paid'}</td>
+                            <td>{order.status}</td>
+                            <td><Button variant="primary" disabled={order.payment?.created}>Pay</Button></td>
+                            {
+                                order.status === 'shipped' ?
+                                    <td><Button variant="warning" disabled>Cancel</Button></td>
+                                    :
+                                    <td><Button onClick={() => handleDelete(order._id)} variant="warning">Cancel</Button></td>
+                            }
+                            <td><Button variant="danger" onClick={() => handleDelete(order._id)}>Delete</Button></td>
+                        </tr>)
+                            :
+                            <>
+                                <Spinner animation="grow" size="sm" />
+                                <Spinner animation="grow" /></>
+                    }
+                </tbody>
+            </Table>
         </div>
     );
 };
