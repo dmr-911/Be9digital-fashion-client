b07222888cca662cdc1ff69d8dce3e6338d4316c
mizan <dewan.mizanur911@gmail.com>
2021-12-25 16:42:04 +0600

    electric product component created

--- src/Pages/Dashboard/ManageProducts/ElectricProduct.js
+++ src/Pages/Dashboard/ManageProducts/ElectricProduct.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
-import './E_product.css';
+import { Button, Card, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import Rating from 'react-rating';
-import { useNavigate } from 'react-router-dom';
 
+const ElectricProducts = ({eProduct}) => {
 
-const E_product = ({product}) => {
     const cart = <FontAwesomeIcon icon={faCartPlus} />
-    const {img, name, price, stock, star, key} = product;
-    const navigate = useNavigate();
+    const {img, name, price, stock, star, key} = eProduct;
     const handleClick = id =>{
-        navigate(`/order/${id}`);
+        // navigate(`/order/${id}`);
     };
 
     return (
         <Col>
-        <Card className="p-3">
-            <Card.Text>
-                <div className="d-flex justify-content-between">
-                    <div className="bg-danger text-white px-2">
-                        <span>5% off</span>
-                    </div>
-                    <span className="far fa-heart favorite"></span>
-                </div>
-            </Card.Text>
+        <Card className="p-2" style={{minHeight: '450px'}}>
             <Card.Img variant="top" src={img} height="140" />
             <Card.Body>
-            <Card.Title>{name.slice(0,24)}</Card.Title>
+            <Card.Title>{name.slice(0,20)}</Card.Title>
             <Card.Text>
                 <div className="d-flex justify-content-between">
                     <div className="text-start">
@@ -47,11 +36,11 @@ const E_product = ({product}) => {
                     </div>
                 </div>
             </Card.Text>
-            <button className="btn-products" onClick={()=>handleClick(key)}><span className="me-2">{cart}</span>Order Now</button>
+            <Button variant="danger" onClick={()=>handleClick(key)}>Delete</Button>
             </Card.Body>
         </Card>
         </Col>
     );
 };
 
-export default E_product;
+export default ElectricProducts;
--- src/Pages/Dashboard/ManageProducts/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts/ManageProducts.js
@@ -1,12 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Row, Table } from 'react-bootstrap';
 import useAuth from '../../../hooks/useAuth';
+import ElectricProducts from './ElectricProduct';
 import './ManageProducts.css';
 
 const ManageProducts = () => {
+    const [eProducts, setEProducts] = useState([]);
+    const [glasses, setGlasses] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/e_products')
+        .then(res => res.json())
+        .then(data => setEProducts(data))
+    },[]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/glasses')
+        .then(res => res.json())
+        .then(data => setGlasses(data))
+    },[])
 
     return (
-       <div></div>
+       <div>
+           <Row sm={1} md={3} lg={4} className="g-3">
+           {
+               eProducts.map(eProduct => <ElectricProducts
+               key={eProduct._id}
+               eProduct = {eProduct}
+               ></ElectricProducts>)
+           }
+           </Row>
+       </div>
     );
 };
 
