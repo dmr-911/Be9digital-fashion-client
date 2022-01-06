eb76807648ff652f6cc56932ee81cd3d5c8b74c8
mizan <dewan.mizanur911@gmail.com>
2022-01-06 07:35:36 +0600

    navbar updated

--- src/Pages/AdminRoute/AdminRoute.js
+++ src/Pages/AdminRoute/AdminRoute.js
@@ -8,7 +8,12 @@ const AdminRoute = ({children}) => {
     const {user, admin} = useAuth();
     const location = useLocation();
     if (!admin) {
-      return <Spinner animation="border" />;
+      <div className="d-flex justify-content-center align-items-center w-100" style={{height: '80vh'}}>
+      <div>
+      <Spinner animation="grow" size="sm" />
+      <Spinner animation="grow" />
+      </div>
+       </div>
     }
   return user.email && admin ? children : <Navigate to="/"  state={{ from: location }}/>;
 };
